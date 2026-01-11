12d389a9447dcfac32d9ebf1af13d2b4c9cca81b
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-01-11 19:50:44 +0530

    reverse a number

---
+++ reverseNo.java
@@ -0,0 +1,33 @@
+public class reverseNo {
+
+      public static void main(String[] args) {
+
+        int x = -124;
+        int n=0;
+        boolean negative = false;
+        if(x < 0){
+            x = x+(x*-2);
+            negative = true;
+        }
+        while (x>0) {
+            n = n*10;
+            n += x%10;
+            x= x/10;
+        }
+        if(negative){
+            n = n*-1;
+        }
+        System.out.println(n);
+
+        
+        // Integer n = 1234;
+        // String str ="";
+        // String s = n.toString(n);
+        // for(int i=s.length()-1;i>=0;i--){
+        //     // System.out.print(s.charAt(i));
+        //     str = str+s.charAt(i);
+
+        // }
+        // System.out.println(str);
+    }
+}
