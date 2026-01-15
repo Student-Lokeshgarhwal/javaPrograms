13d14ac67de3ab9ecfcebd4ac0a7b6374d9e146d
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-01-15 19:23:41 +0530

    get the first digit of integer value

---
+++ firstDigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class firstDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any integer value");
+        int n = sc.nextInt();
+        int number = n;
+        while (number / 10 != 0) {
+            number = number / 10;
+        }
+        System.out.println("The first digit of integer value is :"+number);
+        return;
+    }
+}
