968fa84e1d2606141c46f99ef3fa79de598e9820
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-04-27 21:18:36 +0530

    Given an array of integers nums, return the number of good pairs.A pair (i, j) is called good if nums[i] == nums[j] and i < j.

---
+++ noOfGoodPairs.java
@@ -0,0 +1,16 @@
+public class noOfGoodPairs {
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i] == nums[j]) count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] num = {1,2,3,1,1,3};
+        int res = numIdenticalPairs(num);
+        System.out.println(res);
+    }
+}
