ed07783658dbbf5cdbe4b541519f0047c83c95f9
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-05-08 23:54:13 +0530

    sort map by key

---
+++ sortByKey.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class sortByKey {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(3, "C");
+        map.put(1, "A");
+        map.put(2, "B");
+
+        // Sort by Key
+        TreeMap<Integer, String> sortedMap = new TreeMap<>(map);
+
+        System.out.println(sortedMap);
+    }
+}
+```
