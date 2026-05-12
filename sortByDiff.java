e657c44f19bdb621226cfde0a60b46f03aa29359
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-05-12 23:58:58 +0530

    sort

---
+++ sortByDiff.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class sortByDiff {
+
+    public static int minimumEffort(int[][] tasks) {
+
+        Arrays.sort(tasks, (a, b) ->
+            (b[1] - b[0]) - (a[1] - a[0])
+        );
+
+        int ans = 0;
+        int curr = 0;
+
+        for (int[] task : tasks) {
+
+            int actual = task[0];
+            int minimum = task[1];
+
+            if (curr < minimum) {
+
+                ans += (minimum - curr);
+
+                curr = minimum;
+            }
+
+            curr -= actual;
+        }
+
+        return ans;
+    }
+    
+}
